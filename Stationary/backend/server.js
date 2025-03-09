a2d784d75ea9a8123d35cc21716b77ed454f14ea
jaykerkar0405 <bytewise0405@gmail.com>
2025-03-09 13:45:00 +0530

    Code Cleanup

--- Stationary/backend/server.js
+++ Stationary/backend/server.js
@@ -73,5 +73,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/stationary", stationaryRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

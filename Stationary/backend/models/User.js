3373c6b8c7cab3e242ba940492256fd51d015e3e
jaykerkar0405 <bytewise0405@gmail.com>
2025-03-18 19:09:05 +0530

    Added field to store user's FCM token

--- Stationary/backend/models/User.js
+++ Stationary/backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  fcmToken: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
 });
 
